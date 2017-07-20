14b7fe84a743dfdbeb89d9840b2359e90c30b887
Josh Smith <joshdotsmith@gmail.com>
2017-07-19 17:08:58 -0700

    Add start of github-app-installation component and Mirage setup

--- app/models/github-app-installation.js
+++ app/models/github-app-installation.js
@@ -3,6 +3,10 @@ import attr from 'ember-data/attr';
 import { belongsTo, hasMany } from 'ember-data/relationships';
 
 export default Model.extend({
+  githubAccountAvatarUrl: attr(),
+  githubAccountId: attr(),
+  githubAccountLogin: attr(),
+  githubAccountType: attr(),
   githubId: attr(),
   insertedAt: attr(),
   installed: attr(),
--- mirage/models/organization.js
+++ mirage/models/organization.js
@@ -1,6 +1,7 @@
 import { Model, belongsTo, hasMany } from 'ember-cli-mirage';
 
 export default Model.extend({
+  organizationGithubAppInstallations: hasMany(),
   owner: belongsTo('user'),
   projects: hasMany(),
   stripeConnectAccount: belongsTo()
