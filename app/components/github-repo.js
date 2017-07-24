228575979ae07a6aeb8a7297ab10a9f8c7862627
Josh Smith <joshdotsmith@gmail.com>
2017-07-23 17:42:57 -0700

    Add proper project github repo relationships to component

--- app/components/github-repo.js
+++ app/components/github-repo.js
@@ -3,20 +3,46 @@ import recordsList from 'code-corps-ember/utils/records-list';
 
 const {
   computed,
+  computed: { alias, filter, mapBy, notEmpty },
   Component,
-  get
+  get,
+  inject: { service }
 } = Ember;
 
 export default Component.extend({
   classNames: ['github-repo'],
+  classNameBindings: ['isConnected:github-repo--connected'],
   tagName: 'li',
 
-  isConnected: computed('githubRepo', 'project.projectGithubRepos.@each', function() {
-    let githubRepo = get(this, 'githubRepo');
-    let project = get(this, 'project');
-    let projectGithubRepos = get(this, 'project.projectGithubRepos');
-    if (projectGithubRepos) {
-      return recordsList.includes(projectGithubRepos, githubRepo);
+  githubRepo: null,
+  project: null,
+
+  store: service(),
+
+  isConnected: notEmpty('projectsGithubRepo'),
+  projectGithubRepo: computed('projectGithubRepos.@each', 'projectGithubRepos.isFulfilled', 'githubRepo.id', function() {
+    return get(this, 'projectGithubRepos').find((projectGithubRepo) => {
+      return projectGithubRepo.belongsTo('githubRepo').id() === get(this, 'githubRepo.id');
+    });
+  }),
+  projectGithubRepos: alias('project.projectGithubRepos'),
+  projectsGithubRepo: computed('projectsGithubRepos.@each', 'projectsGithubRepos.@each.isFulfilled', 'githubRepo', function() {
+    let projectsGithubRepos = get(this, 'projectsGithubRepos');
+    return projectsGithubRepos.find((githubRepo) => {
+      return get(githubRepo, 'id') === get(this, 'githubRepo.id');
+    });
+  }),
+  projectsGithubRepos: mapBy('projectGithubRepos', 'githubRepo'),
+
+  actions: {
+    connect() {
+      let githubRepo = get(this, 'githubRepo');
+      let project = get(this, 'project');
+      return get(this, 'store').createRecord('project-github-repo', { project, githubRepo }).save();
+    },
+
+    remove(projectGithubRepo) {
+      return projectGithubRepo.destroyRecord();
     }
-  })
+  }
 });
